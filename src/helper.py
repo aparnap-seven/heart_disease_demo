206c64c10b77d446751b2526b82d5cf98a0a5100
aparnap-seven <aparnapseven@gmail.com>
2026-05-15 12:38:13 +0530

    updated src

--- src/helper.py
+++ src/helper.py
@@ -6,6 +6,6 @@ config_name = "config.yaml"
 
 
 def load_config(config_name=config_name):
-    with open(join(CONFIG_PATH/ config_name), "r") as f:
+    with open(join(CONFIG_PATH, config_name), "r") as f:
         config = yaml.safe_load(f)
     return config
