c78d8aebef154a483e3b28e2dc49c26f7d0e601b
aparnap-seven <aparnapseven@gmail.com>
2026-05-15 13:15:30 +0530

    updated main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from src.helper import load_config
+from src.data_preprocessing import data_preprocessing
+from src.modeling import train_model, evaluate_model, save_model
+
+config = load_config()
+
+raw_data_path = config["raw_data_path"]
+data = pd.read_csv(raw_data_path)
+
+data = data_preprocessing(data)
+data.to_csv(config["preprocessed_data_path"], index=False)
+
+model, X_test, y_test = train_model(data)
+evaluate_model(model, X_test, y_test)
+save_model(model, config["model_save_path"])
