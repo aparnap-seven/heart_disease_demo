afb9d97bbe526608c01ad4df6db9cc8eee3c721c
aparnap-seven <aparnapseven@gmail.com>
2026-05-15 12:23:18 +0530

    added src

---
+++ src/helpe.py
@@ -0,0 +1,11 @@
+import yaml
+from os.path import join
+
+CONFIG_PATH = "config"
+config_name = "config.yaml"
+
+
+def load_config(config_name=config_name):
+    with open(CONFIG_PATH/ config_name, "r") as f:
+        config = yaml.safe_load(f)
+    return config
